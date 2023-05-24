5f17a11e098004cda010a1e6e343b9fd0557ce6d
Santosh <santoshkushwah01012001@gmail.com>
2023-05-24 20:50:28 +0530

    express completed

--- api.js
+++ api.js
@@ -3,16 +3,21 @@ const path = require("path");
 const bodyParser = require("body-parser")
 const router = require("./userRoutes");
 
-
 const app = express();
 const port = 4000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json())
-app.use("/api/v1", router);
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"));
+app.get("/api/v1/userdata",(req,res)=>{
+    res.json({
+        name: "Santosh",
+        email: "Sample@gmail.com",
+        password: "hexed",
+    });
 });
+
+app.post("/api/v1/register",)
+
 
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`);
